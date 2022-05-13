ef5e5faf3f575b214093db77c8fbe195e48b69bb
kulkarni2u <kulkarni2u@gmail.com>
2022-05-12 21:02:13 -0700

    Initial Checkin

---
+++ src/test/java/com/casper/springnativedemo/SpringNativeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casper.springnativedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
